d765148b9c7a5b310c9bd5b218c974860f1a96bf
Artur Matysiak <inz.matys@gmail.com>
2023-12-02 15:39:11 +0100

    task done

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,13 @@
-const n = 24;
+const n = 100;
 const oddNumbers = [];
+
+fillArrayOddNumbers = () => {
+  for (i = 0; i < n; i++) {
+    const number = (Math.random() * n).toFixed();
+    if (number % 2 !== 0) oddNumbers[i] = number;
+    else i--;
+  }
+};
+
+fillArrayOddNumbers();
+console.log(oddNumbers);
