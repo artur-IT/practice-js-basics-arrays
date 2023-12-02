d35554f1ee96783cc3bb99fe4cb75bdb718fa4f2
Artur Matysiak <inz.matys@gmail.com>
2023-12-02 14:08:03 +0100

    task done

---
+++ 01/app.js
@@ -0,0 +1,7 @@
+const users = ["artur matysiak", "kasia matysiak", "jan kowal", "john wick", "rocky balboa"];
+
+console.log(users[0]);
+console.log(users[2]);
+console.log(users[4]);
+
+console.log(users.length);
