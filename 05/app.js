a7a48a7d1d35a1be3504a1f1093368c2c8d4a850
Artur Matysiak <inz.matys@gmail.com>
2023-12-02 16:07:10 +0100

    task done

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,11 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+sum = () => {
+  const evenTab = numbers.filter((number) => number % 2);
+  const sum = evenTab.reduce((accumulator, item) => accumulator + item);
+  console.log(evenTab);
+  return sum;
+};
+
+console.log(sum());
+// sum()
